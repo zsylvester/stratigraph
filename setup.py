1085bc5fd13120049cd7a7cf9ff2b026e0fd0f28
Zoltan Sylvester <zoltan.sylvester@gmail.com>
2024-04-29 16:43:37 -0500

    switch back to setup.py so that pypi.org distribution actually works

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+long_description = """\
+'stratigraph' is a Python package for stratigraphic visualization and analysis
+"""
+
+setuptools.setup(
+    name="stratigraph",
+    version="0.1.1",
+    author="Zoltan Sylvester",
+    author_email="zoltan.sylvester@beg.utexas.edu",
+    description="A Python module for stratigraphic visualization and analysis",
+    keywords = "geology, geoscience, stratigraphy, chronostratigraphy, visualization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zsylvester/stratigraph",
+    packages=['stratigraph'],
+    install_requires=["python>=3.8", "numpy", "scipy", "scikit-learn", "scikit-image", 
+        "shapely", "pillow", "matplotlib", "mayavi", "tqdm"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
